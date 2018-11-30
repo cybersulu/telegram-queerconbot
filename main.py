1d32b5a60a9e3b29b9a26a4c99b5a176f1430caf
Peter Teoh <pete@teoh.us>
2018-11-30 05:42:25 -0500

    add comment

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
 
+# list of diabetus images
 diabetus = ["https://media1.tenor.com/images/38b0f21d0e76dec2ff58d19e37fcc716/tenor.gif?itemid=4484736",
         "https://1funny.com/wp-content/uploads/2009/07/diabeetus-cat.jpg",
         "http://rs367.pbsrc.com/albums/oo112/Aim_fire/sdgfasfdgd.jpg~c200"]
@@ -13,8 +14,6 @@ def webhook(request):
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.effective_message.chat.id
         messagetext = update.effective_message.text
-        # Reply with the same message
-        # bot.sendMessage(chat_id=chat_id, text=update.message.text)
         if "backpack" in messagetext.lower():
             bot.sendMessage(chat_id=chat_id, text="https://media1.tenor.com/images/3862340576b167181f07a120e11a400b/tenor.gif?itemid=8722064")
         if "chika" in messagetext.lower() or "chikalicious" in messagetext.lower():
