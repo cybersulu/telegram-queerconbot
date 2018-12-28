9e9cfd681b1cb763e433951dd388cf03caa7d590
Peter Teoh <pete@teoh.us>
2018-12-27 22:08:24 -0500

    add trailing commas

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ backpack = [
     "https://media1.tenor.com/images/3862340576b167181f07a120e11a400b/tenor.gif?itemid=8722064",
     "Mmm... yeah... the pack for the back.", "I like turtles.", "I like pie.",
     "Das ist ein rucksack auf Deutsch!",
-    "Oh, and remember, next Friday is Swedish luggage day, so, you know, if you want to, go ahead and wear a bäckpäck."
+    "Oh, and remember, next Friday is Swedish luggage day, so, you know, if you want to, go ahead and wear a bäckpäck.",
 ]
 
 # list of diabetus images
@@ -18,7 +18,7 @@ diabetus = [
     "https://media1.tenor.com/images/38b0f21d0e76dec2ff58d19e37fcc716/tenor.gif?itemid=4484736",
     "https://1funny.com/wp-content/uploads/2009/07/diabeetus-cat.jpg",
     "http://rs367.pbsrc.com/albums/oo112/Aim_fire/sdgfasfdgd.jpg~c200",
-    "https://c1.staticflickr.com/3/2254/2334517660_c5a9522dbd.jpg"
+    "https://c1.staticflickr.com/3/2254/2334517660_c5a9522dbd.jpg",
 ]
 
 # list of catfacts
@@ -73,14 +73,14 @@ catfacts = [
     "(47) On average, cats spend 2/3 of every day sleeping. That means a nine-year-old cat has been awake for only three years of its life.",
     "(48) In the original Italian version of Cinderella, the benevolent fairy godmother figure was a cat.",
     "(49) The little tufts of hair in a cat’s ear that help keep out dirt direct sounds into the ear, and insulate the ears are called “ear furnishings.”",
-    "(50) The ability of a cat to find its way home is called “psi-traveling.” Experts think cats either use the angle of the sunlight to find their way or that cats have magnetized cells in their brains that act as compasses."
+    "(50) The ability of a cat to find its way home is called “psi-traveling.” Experts think cats either use the angle of the sunlight to find their way or that cats have magnetized cells in their brains that act as compasses.",
 ]
 
 # list of dumpster fires
 dumpsterfire = [
     "https://media.giphy.com/media/QLyhWVTvAHbAbAdWcp/giphy.gif",
     "https://media.giphy.com/media/134vVkHV9wQtaw/giphy.gif",
-    "https://media.giphy.com/media/FqtWrearu5vb2/giphy.gif"
+    "https://media.giphy.com/media/FqtWrearu5vb2/giphy.gif",
 ]
 
 
